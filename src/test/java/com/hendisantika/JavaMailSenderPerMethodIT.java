cda837c28dbb5e62c374e4f9e7f709b7d6dc61dd
Hendi Santika <hendisantika@yahoo.co.id>
2022-06-22 06:53:19 +0700

    #13 - Add JavaMailSenderPerMethodIT

---
+++ src/test/java/com/hendisantika/JavaMailSenderPerMethodIT.java
@@ -0,0 +1,31 @@
+package com.hendisantika;
+
+import com.icegreen.greenmail.configuration.GreenMailConfiguration;
+import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.util.ServerSetupTest;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-mail-sending
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/22/22
+ * Time: 06:52
+ * To change this template use File | Settings | File Templates.
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class JavaMailSenderPerMethodIT {
+
+    @RegisterExtension
+    static GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP)
+            .withConfiguration(GreenMailConfiguration.aConfig().withUser("duke", "springboot"))
+            .withPerMethodLifecycle(true);
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+}
