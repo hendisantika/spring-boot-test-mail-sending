804d0b387a7aacc0e8113b209e9d9a41ed861aa1
Hendi Santika <hendisantika@yahoo.co.id>
2022-06-22 06:59:11 +0700

    #19 - Add NotificationControllerIT

---
+++ src/test/java/com/hendisantika/NotificationControllerIT.java
@@ -0,0 +1,31 @@
+package com.hendisantika;
+
+import com.icegreen.greenmail.configuration.GreenMailConfiguration;
+import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.util.ServerSetupTest;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-mail-sending
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/22/22
+ * Time: 06:58
+ * To change this template use File | Settings | File Templates.
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class NotificationControllerIT {
+
+    @RegisterExtension
+    static GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP)
+            .withConfiguration(GreenMailConfiguration.aConfig().withUser("naruto", "konoha"))
+            .withPerMethodLifecycle(false);
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+}
