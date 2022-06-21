cd53bf1e612e328a2e1525cef38585a287e25ad5
Hendi Santika <hendisantika@yahoo.co.id>
2022-06-22 06:47:32 +0700

    #11 - Add GreenMail Configs in SpringBootTestMailSendingApplicationTests

--- src/test/java/com/hendisantika/SpringBootTestMailSendingApplicationTests.java
+++ src/test/java/com/hendisantika/SpringBootTestMailSendingApplicationTests.java
@@ -1,10 +1,19 @@
 package com.hendisantika;
 
+import com.icegreen.greenmail.configuration.GreenMailConfiguration;
+import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.util.ServerSetupTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootTestMailSendingApplicationTests {
+    @RegisterExtension
+    static GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP)
+            .withConfiguration(GreenMailConfiguration.aConfig().withUser("naruto", "konoha"))
+            .withPerMethodLifecycle(false);
+
 
     @Test
     void contextLoads() {
